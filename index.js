ff83b0ad5066f193b95e7fb29a0d31a50debea4a
jdrome <josedanilromero@gmail.com>
2025-08-12 13:43:22 -0500

    refactor(frontend): reduce button border thickness

--- index.js
+++ index.js
@@ -14,10 +14,28 @@ const selectMessage = () => {
         "I deserve self respect and a clean space",
         "I do all things in love",
         "I do not rush through my life, I temper speed with stillness",
-        "I embrace change and rise to the new opportunity it presents",
+        "I embrace change and rise to the new opportunities it presents",
         "I am going further and learning along the way",
         "I love what I love",
-        "I release the fears that do not serve me"
+        "I release the fears that do not serve me",
+        "I welcome wisdom that comes with growing older",
+        "My perspective is important",
+        "My sensitivity is beautiful, and my feelings and emotions are valid",
+        "My weirdness is wonderful",
+        "My rest is valuable and healthy",
+        "I can do hard things, even when I am nervous",
+        "I trust myself",
+        "I am safe",
+        "I treat myself with love and kindness",
+        "I am needed",
+        "I am capable of success",
+        "I exude beauty and grace",
+        "My self-worth is not determined by my accomplishments",
+        "I am worthy of love just for being who I am",
+        "It is okay to take things one step at a time",
+        "I can go at my own pace",
+        "I deserve fulfilling relationships",
+        "I will allow myself to rest when my body needs it"
     ];
 
     const index = Math.floor(Math.random() * affirmations.length);
